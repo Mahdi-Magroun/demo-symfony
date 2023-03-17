cd0d6b75d15336a8220486a9e4529ff077381ccc
Mahdi <mmahdiwebdeveloper@gmail.com>
2023-03-17 17:12:29 +0100

    get one president with detail

--- src/Manager/MunicipalityPresidentManager.php
+++ src/Manager/MunicipalityPresidentManager.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Manager;
 
+use App\Entity\MunicipalityAgent;
 use Doctrine\Persistence\ManagerRegistry;
 use SSH\MyJwtBundle\Manager\ExceptionManager;
 use Symfony\Component\Security\Core\Security;
@@ -23,10 +24,47 @@ class MunicipalityPresidentManager extends AbstractManager{
     }
     public function init(string $method,$settings=[]){
         parent::setSettings($settings);
-
+        return $this;
     }
     public function getOneDetail($code){
-        
+        $president = $this->apiEntityManager->getRepository(MunicipalityAgent::class)->findOneBy([
+            "role"=>"ROLE_MUNICIPALITY_PRESIDENT",
+            "code"=>$code
+        ]);
+        if (!$president) {
+           throw new \Exception("no_municipality_president_found", 1);
+           
+        }
+        return [
+            "code"=>$president->getCode(),
+            "first_name"=>$president->getFirstName(),
+            "last_name"=>$president->getLastName(),
+            "email"=>$president->getEmail(),
+            "cin"=>$president->getCin(),
+            "created_at"=>null,
+            "updated_at"=>null,
+            "is_activated"=>$president->isIsActivated(),
+            "municipality"=>[
+                "code"=>$president->getMunicipality()->getCode(),
+                "frensh_name"=>$president->getMunicipality()->getFrenshName(),
+                "arabic_name"=>$president->getMunicipality()->getArabicName(),
+            ]
+        ];
     }
 
+    public function getMany(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function activation(){
+        
+    }
 }
